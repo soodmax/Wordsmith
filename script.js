481f96b99fffa180aea0b2c53b833ca4fdbb105c
Atul Sood <atul_is_in@hotmail.com>
2020-10-15 13:17:43 -0300

    no message

--- script.js
+++ script.js
@@ -2,31 +2,38 @@ function scan() {
     var chosenWordInput = document.getElementById("chosenWord");
     var wordToScanInput = document.getElementById("wordChallenge");
     var charsCountInput = document.getElementById("charsCount");
+    var charsFoundInput = document.getElementById("charsFound");
     var historyListSelect = document.getElementById("historyList");
 
     var chosenWord = chosenWordInput.value.toLowerCase();
     var wordToScan = wordToScanInput.value.toLowerCase();
 
-    var alpha;
+    var alpha = [];
     var counter = 0;
 
     for (var i = 0; i < wordToScan.length; i++) {
-        console.log(wordToScan.charAt(i));
 
         if (chosenWord.includes(wordToScan.charAt(i)))
         {
-            // alpha.push(wordToScan.charAt(i));
+            alpha.push(wordToScan.charAt(i));
             counter++;
         }
     }
-    charsCountInput.value = counter;
-    historyListSelect.options.add(new Option(chosenWord + " >> " + wordToScan + " >> " + counter, counter), historyListSelect.options[0]);
+    
+    var alphaUnique = alpha.filter(function(item, pos){
+        return alpha.indexOf(item)== pos; 
+      });
+
+    charsCountInput.value = alphaUnique.length;
+    charsFoundInput.value = alphaUnique;
+
+    historyListSelect.options.add(new Option(chosenWord + " >> " + wordToScan + " >> " + alphaUnique.length, alphaUnique.length), historyListSelect.options[0]);
 }
 
 function reset() {
     var wordToScanInput = document.getElementById("wordChallenge");
     var charsCountInput = document.getElementById("charsCount");
-    var charsFoundInput = document.getElementById("charsCount");
+    var charsFoundInput = document.getElementById("charsFound");
 
     wordToScanInput.value = "";
     charsCountInput.value = "";
